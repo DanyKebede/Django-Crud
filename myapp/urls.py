2eea7a9901c55e3752e4a35ae76d541bc2e109d1
nabroleonx <nabroleonx@gmail.com>
2023-05-30 23:40:50 +0300

    add auth for admin

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import HospitalViewSet, DoctorViewSet
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'hospitals', HospitalViewSet)
@@ -8,4 +9,5 @@ router.register(r'doctors', DoctorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', views.obtain_auth_token, name='api-token-auth'),
 ]
