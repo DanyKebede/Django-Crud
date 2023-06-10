9d9c333679ffb4f92682cf45042bdbdd12a6afbd
danykebede <bkdyfzw12@gmail.com>
2023-06-10 16:28:23 +0300

    Clean up

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import HospitalViewSet, DoctorViewSet
+from .views import HospitalViewSet, DoctorViewSet, change_superuser_password_api, checkdisease
 from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
@@ -10,4 +10,6 @@ router.register(r'doctors', DoctorViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', views.obtain_auth_token, name='api-token-auth'),
+    path('change_password/', change_superuser_password_api, name='change_password'),
+    path('checkdisease', checkdisease, name="checkdisease"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,190 @@
-from rest_framework import viewsets, permissions
+import joblib as jb
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+import json
+
+
 from .models import Hospital, Doctor
 from .serializers import HospitalSerializer, DoctorSerializer
-
 
 class HospitalViewSet(viewsets.ModelViewSet):
     queryset = Hospital.objects.all()
     serializer_class = HospitalSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+@csrf_exempt
+@api_view(['POST'])
+def change_superuser_password_api(request):
+    if not request.user.is_authenticated or not request.user.is_superuser:
+        return JsonResponse({'error': 'Unauthorized access.'}, status=401)
+
+    old_password = request.POST.get('old_password')
+    new_password = request.POST.get('new_password')
+    confirm_password = request.POST.get('confirm_password')
+
+    superuser = User.objects.get(username=request.user.username)
+
+    if not superuser.check_password(old_password):
+        return JsonResponse({'error': 'Old password is incorrect.'}, status=400)
+
+    if new_password != confirm_password:
+        return JsonResponse({'error': 'New password and confirm password do not match.'}, status=400)
+
+    superuser.set_password(new_password)
+    superuser.save()
+
+    return JsonResponse({'success': 'Password changed successfully.'}, status=200)
+
+#loading trained_model
+model = jb.load('trained_model')
+
+def checkdisease(request):
+
+  symptomslist=['itching','skin_rash','nodal_skin_eruptions','continuous_sneezing','shivering','chills','joint_pain',
+  'stomach_pain','acidity','ulcers_on_tongue','muscle_wasting','vomiting','burning_micturition','spotting_ urination',
+  'fatigue','weight_gain','anxiety','cold_hands_and_feets','mood_swings','weight_loss','restlessness','lethargy',
+  'patches_in_throat','irregular_sugar_level','cough','high_fever','sunken_eyes','breathlessness','sweating',
+  'dehydration','indigestion','headache','yellowish_skin','dark_urine','nausea','loss_of_appetite','pain_behind_the_eyes',
+  'back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
+  'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
+  'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
+  'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
+  'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
+  'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
+  'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
+  'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
+  'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
+  'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
+  'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
+  'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
+  'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
+  'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
+  'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
+  'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
+  'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
+  'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
+  'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
+  'yellow_crust_ooze']
+
+   #  if request.method == 'POST':
+   #      # Parse the JSON data from the request body
+   #      data = json.loads(request.body)
+
+   #      inputno = int(data["noofsym"])
+   #      print(inputno)
+   #      if (inputno == 0):
+   #          return JsonResponse({'predicteddisease': "none", 'confidencescore': 0})
+
+   #      else:
+   #          psymptoms = data["symptoms"]
+   #          # Convert the comma-separated string back to a list
+   #          psymptoms = psymptoms.split(',')
+
+  if request.method == 'POST':
+      data = json.loads(request.body)
+      inputno = int(data["noofsym"])
+      # print(inputno)
+      if (inputno == 0 ) :
+          return JsonResponse({'predicteddisease': "none",'confidencescore': 0 })
+  
+      else :
+
+        psymptoms = data["symptoms"]
+        psymptoms = psymptoms.split(',')
+       
+      #   print(psymptoms)
+      
+        testingsymptoms = []
+        #append zero in all coloumn fields...
+        for x in range(0, len(symptomslist)):
+          testingsymptoms.append(0)
+
+
+        #update 1 where symptoms gets matched...
+        for k in range(0, len(symptomslist)):
+
+          for z in psymptoms:
+              if (z == symptomslist[k]):
+                  testingsymptoms[k] = 1
+
+
+        inputtest = [testingsymptoms]
+
+      #   prints(inputtest)
+      
+
+        predicted = model.predict(inputtest)
+      #   print("predicted disease is : ")
+      #   print(predicted)
+
+        y_pred_2 = model.predict_proba(inputtest)
+        confidencescore=y_pred_2.max() * 100
+      #   print(" confidence score of : = {0} ".format(confidencescore))
+
+        confidencescore = format(confidencescore, '.0f')
+        predicted_disease = predicted[0]
+
+        Rheumatologist = [  'Osteoarthristis','Arthritis']
+       
+        Cardiologist = [ 'Heart attack','Bronchial Asthma','Hypertension ']
+       
+        ENT_specialist = ['(vertigo) Paroymsal  Positional Vertigo','Hypothyroidism' ]
+
+        Orthopedist = []
+
+        Neurologist = ['Varicose veins','Paralysis (brain hemorrhage)','Migraine','Cervical spondylosis']
+
+        Allergist_Immunologist = ['Allergy','Pneumonia',
+        'AIDS','Common Cold','Tuberculosis','Malaria','Dengue','Typhoid']
+
+        Urologist = [ 'Urinary tract infection',
+         'Dimorphic hemmorhoids(piles)']
+
+        Dermatologist = [  'Acne','Chicken pox','Fungal infection','Psoriasis','Impetigo']
+
+        Gastroenterologist = ['Peptic ulcer diseae', 'GERD','Chronic cholestasis','Drug Reaction','Gastroenteritis','Hepatitis E',
+        'Alcoholic hepatitis','Jaundice','hepatitis A',
+         'Hepatitis B', 'Hepatitis C', 'Hepatitis D','Diabetes ','Hypoglycemia']
+         
+        if predicted_disease in Rheumatologist :
+           consultdoctor = "Rheumatologist"
+           
+        if predicted_disease in Cardiologist :
+           consultdoctor = "Cardiologist"
+           
+
+        elif predicted_disease in ENT_specialist :
+           consultdoctor = "ENT specialist"
+     
+        elif predicted_disease in Orthopedist :
+           consultdoctor = "Orthopedist"
+     
+        elif predicted_disease in Neurologist :
+           consultdoctor = "Neurologist"
+     
+        elif predicted_disease in Allergist_Immunologist :
+           consultdoctor = "Allergist/Immunologist"
+     
+        elif predicted_disease in Urologist :
+           consultdoctor = "Urologist"
+     
+        elif predicted_disease in Dermatologist :
+           consultdoctor = "Dermatologist"
+     
+        elif predicted_disease in Gastroenterologist :
+           consultdoctor = "Gastroenterologist"
+     
+        else :
+           consultdoctor = "other"
+
+
+        return JsonResponse({'predicteddisease': predicted_disease ,'confidencescore':confidencescore , "consultdoctor": consultdoctor})
